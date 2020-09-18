fdfc08b1028ad8afb0c6b460346d4debc2a7415c
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2020-09-18 14:31:11 -0700

    add project

---
+++ CompanionNumberGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CompanionNumberGenerator
+//
+//  Created by Abdulsalam Mansour on 9/18/20.
+//  Copyright © 2020 abdulsalam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
